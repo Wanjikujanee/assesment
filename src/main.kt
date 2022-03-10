96fe39fc2ccaedbe262f90c35b5149b53f1196c4
wanjikujanee <wanjikumaryjanee@gmail.com>
2022-03-10 08:58:18 +0300

    my work

---
+++ src/main.kt
@@ -0,0 +1,27 @@
+import java.sql.Statement
+import kotlin.contracts.Returns
+
+fun main() {
+   var txt:String="codehive"
+    println(listOf(txt[5])[6].[7].[8])
+
+    modulus(num1=38,num2=26)
+var name:String="family"
+    println(name.length)
+var word:String="jane"
+    var age:Int=20
+    println(statement)
+
+
+
+}
+
+
+
+
+fun getremainder(){
+    var modulus=(Num1 % Num2)
+    println(getremainder())
+    return(modulus)
+
+}
